23c09b4dfa5ff6d7d1072319f1cb2a9949948000
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-17 15:47:15 -0500

    countZeroes code

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,13 @@
 function countZeroes() {
-  
+  if (arr.length === 1) {
+    return arr[0] === 0 ? 1 : 0;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const leftZeroes = countZeroes(arr.slice(0, middle));
+  const rightZeroes = countZeroes(arr.slice(middle));
+
+  return leftZeroes + rightZeroes;
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
