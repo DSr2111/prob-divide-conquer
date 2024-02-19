7de8adc800fde2a421220c3df075d31dde76f16d
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-19 13:09:41 -0500

    find rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,21 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  const length = arr.length;
+
+  if (length <= 1) {
+    return 0;
+  }
+
+  if (arr[0] < arr[length - 1]) {
+    return 0;
+  }
+
+  const mid = Math.floor(length / 2);
+
+  if (arr[mid] > arr[length - 1]) {
+    return mid + findRotationCount(arr.slice(mid));
+  } else {
+    return findRotationCount(arr.slice(0, mid + 1));
+  }
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
