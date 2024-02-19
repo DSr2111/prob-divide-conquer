ccba6387ab8cd9e0f9d7a02bb4286fb96e6362f6
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-19 13:00:11 -0500

    rotated index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,28 @@
 function findRotatedIndex() {
- 
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (arr[middle] === num) {
+      return middle;
+    }
+
+    if (arr[start] <= arr[middle]) {
+      if (num >= arr[start] && num < arr[mid]) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    } else {
+      if (num > arr[middle] && num <= arr[end]) {
+        start = middle + 1;
+      } else {
+        end = middle - 1;
+      }
+    }
+  }
+  return -1;
 }
 
-module.exports = findRotatedIndex
+module.exports = findRotatedIndex;
